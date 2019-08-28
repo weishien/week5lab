67917eee3d9aaac5e5f5fa2bf1ab6e67ddb89559
weishien <wcha0031@student.monash.edu>
2019-08-28 21:44:25 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let ejs = require('ejs');
+
+let app = express();
+let db = [];
+
+let viewPaths = __dirname + '/public';
+
+// configure express to handle machine
+app.engine('html',ejs.renderFile);
+app.set('view engine','html');
+
+// parse application
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
+
+app.use(express.static('img'));
+app.use(express.static('css'));
+
+app.get('/', function(req,res) {
+    res.sendFile(viewPaths + '/index.html');
+});
+
+app.get('/listTasks', function(req,res) {
+    res.render(viewPaths + '/listTasks.html', {
+        task : db 
+    })
+});
+
+app.get('/newTask', function(req,res) {
+    res.sendFile(viewPaths + '/newTask.html')
+});
+
+app.listen(8080);
