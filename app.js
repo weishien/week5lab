5de41ada543583c77e904a2bc7ea0464ed81ec65
weishien <wcha0031@student.monash.edu>
2019-08-28 22:10:50 +1000

    add documentation

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let ejs = require('ejs');
 let app = express();
 let db = [];
 
+// initialise local path to access files from different folders
 let viewPaths = __dirname + '/public';
 
 // configure express to handle machine
@@ -16,27 +17,32 @@ app.use(bodyParser.urlencoded({
     extended:false
 }));
 
+// tells express which file to look for static assets
 app.use(express.static('img'));
 app.use(express.static('css'));
 
+// check pathname and return static homepage
 app.get('/', function(req,res) {
     res.sendFile(viewPaths + '/index.html');
 });
 
+// check pathname and return dynamic list tasks page
 app.get('/listTasks', function(req,res) {
     res.render(viewPaths + '/listTasks.html', {
         task : db 
     })
 });
 
+// check pathname and return static page with forms to take input from clients
 app.get('/newTask', function(req,res) {
     res.sendFile(viewPaths + '/newTask.html')
 });
 
+// catch data input by clients and return static list task page
 app.post('/incomingTask', function(req,res) {
     console.log(req.body);
     db.push(req.body);
-    console.log(db);
+    // console.log(db);
     res.render(viewPaths + '/listTasks.html', { task : db });
 });
 
